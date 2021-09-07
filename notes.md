6b48da9b902f1163865d24401c1361389bda6349
Callum Oakley <hello@callumoakley.net>
2021-09-08 00:39:14 +0100

    code 7

--- notes.md
+++ notes.md
@@ -1,6 +1,6 @@
 # Notes
 
-Puzzle in the ruins:
+## Puzzle in the ruins
 
     _ + _ * _^2 + _^3 - _ = 399
 
@@ -14,11 +14,50 @@ Have to place the coins with the correct values in the slots in order?
 
 so 9,2,5,7,3 or blue, red, shiny, concave, corroded.
 
+## Hacking the teleporter
+
+The check is at address 5489. There's a call to 6027, which leaves some result
+in r0. If this result is 6, we successfully teleport to the beach. We can
+bypass the check and jump straight after the call and jf with
+
+    #(update % :memory assoc 5489 6 5490 5498)
+
+but we still need to figure out what value of r7 will return a 6 from 6027.
+
+Decompiling the code, writing it out in clojure and gradually transforming it
+to be highter level... it turns out to be Ackerman's function! Testing each
+input in turn eventually gives us 25734 as a value of r7 which will result in
+a check of 6.
+
+    #(update % :registers assoc 7 25734)
+
+## Orb puzzle
+
+After messing with the VM in the last puzzle, my first thought was to hack this
+one as well. Changing a jt to a jf after the eq r0 30 check seems to do the
+trick...
+
+    #(update % :memory assoc 4557 8)
+
+But then the code you get is wrong, so it must depend on the state in some way
+that I'm violating. Time to solve it properly I guess.
+
+The floor plan is
+
+    *   8   -   1
+    4   *  11   *
+    +   4   -  18
+    22  -   9   *
+
+with the start at 22, and the vault at 1. We need the orb to weigh 30 when we
+reach the vault.
+
 ## Codes
 
-- SpKPqHYzYxUp given in the hints in arch-spec
-- noEYBZZSyezn VM prints this after implementing 19 and 21
-- lxJymKTjTAso VM prints this after completing self-test
-- FfJqweDRnEuo obtained by writing on the tablet
-- QvZJEumZbLqY chiseled on the wall in the Twisty passages
-- EfGriRahcFsv written in the stars when teleporting
+- `SpKPqHYzYxUp` given in the hints in arch-spec
+- `noEYBZZSyezn` VM prints this after implementing 19 and 21
+- `lxJymKTjTAso` VM prints this after completing self-test
+- `FfJqweDRnEuo` obtained by writing on the tablet
+- `QvZJEumZbLqY` chiseled on the wall in the Twisty passages
+- `EfGriRahcFsv` written in the stars when teleporting
+- `nWMQDVBYuKdT` found after hacking the teleporter
--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,5 @@
-(ns vm
+(ns main
   (:require
-   [clojure.data :as data]
    [clojure.java.io :as io]
    [clojure.string :as str])
   (:import java.lang.Byte))
@@ -109,9 +108,9 @@
 (defn read-line* []
   (if-let [line (first @input)]
     (do
-     (println line)
-     (swap! input rest)
-     line)
+      (println line)
+      (swap! input rest)
+      line)
     (read-line)))
 
 (defn queue-input [vm]
@@ -131,21 +130,54 @@
   (update vm :head + 1))
 
 (defn do-op [vm [op & args]]
-  (apply op vm (map-indexed (fn [i arg] (arg vm (inc i))) args)))
+  (let [vm* (apply op vm (map-indexed (fn [i arg] (arg vm (inc i))) args))]
+    (when (:debug vm)
+      (println)
+      (Thread/sleep 5))
+    vm*))
 
 (defn rarg [vm offset]
+  (when (:debug vm)
+    (print (str "\tr" (- ((:memory vm) (+ offset (:head vm))) 32768))))
   (- ((:memory vm) (+ offset (:head vm))) 32768))
 
 (defn varg [vm offset]
   (let [literal ((:memory vm) (+ offset (:head vm)))]
     (if (< literal 32768)
-      literal
-      ((:registers vm) (rarg vm offset)))))
+      (do
+        (when (:debug vm)
+          (print (str "\t" literal)))
+        literal)
+      (do
+        (when (:debug vm)
+          (print (str "\tr" (- literal 32768)
+                      "(" ((:registers vm) (- literal 32768)) ")")))
+        ((:registers vm) (- literal 32768))))))
+
+(def debug-op-names
+  {0  "halt" 1  "set"  2  "push" 3  "pop"
+   4  "eq"   5  "gt"   6  "jmp"  7  "jt"
+   8  "jf"   9  "add"  10 "mult" 11 "mod"
+   12 "and"  13 "or"   14 "not"  15 "rmem"
+   16 "wmem" 17 "call" 18 "ret"  19 "out"
+   20 "in"   21 "noop"})
+
+(def debug-op-args
+  {0  0      1  2      2  1      3  1
+   4  3      5  3      6  1      7  2
+   8  2      9  3      10 3      11 3
+   12 3      13 3      14 2      15 2
+   16 2      17 1      18 0      19 1
+   20 1      21 0})
 
 (defn run [vm]
+  (when (:debug vm)
+    (print (str "[" (str/join "\t" (:registers vm)) "]" "\t"
+                (:head vm) "\t"
+                (debug-op-names ((:memory vm) (:head vm))))))
   (if (:halted vm)
     vm
-    (recur (do-op vm (case (varg vm 0)
+    (recur (do-op vm (case ((:memory vm) (:head vm))
                        0  [halt-op]                 1 [set-op  rarg varg]
                        2  [push-op varg]            3 [pop-op  rarg]
                        4  [eq-op   rarg varg varg]  5 [gt-op   rarg varg varg]
@@ -158,15 +190,56 @@
                        18 [ret-op]                 19 [out-op  varg]
                        20 [in-op   rarg]           21 [noop-op])))))
 
-(run
- {:memory (->> (io/input-stream "challenge.bin")
-               .readAllBytes
-               (map #(Byte/toUnsignedInt %))
-               (partition 2)
-               (reduce (fn [[m i] [low high]]
-                         [(assoc m i (+ (bit-shift-left high 8) low)) (inc i)])
-                       [(vec (replicate 32768 0)) 0])
-               first)
-  :registers [0 0 0 0 0 0 0 0]
-  :stack []
-  :head 0})
+(defn darg [literal]
+  (if (< literal 32768)
+    literal
+    (str "r" (- literal 32768))))
+
+(defn decompile [memory]
+  (loop [head 0]
+    (if-let [op (debug-op-names (memory head))]
+      (let [args (debug-op-args (memory head))
+            head* (+ head args 1)]
+        (println (str/join "\t" (into [head op]
+                                      (map #(darg (memory (+ head 1 %)))
+                                           (range args)))))
+        (when (< head* (count memory))
+          (recur head*)))
+      (do
+        (println head "\tdata\t" (memory head))
+        (when (< (inc head) (count memory))
+          (recur (inc head)))))))
+
+;; 25734
+;; It's the Ackermann function!
+;; TODO this is still fairly slow. Can we make it faster?
+(defn fast-check []
+  (loop [r7 0]
+    (def f6027
+      (memoize
+       (fn [r0 r1 r7]
+         (if (zero? r0)
+           (mod (inc r1) 32768)
+           (if (zero? r1)
+             (f6027 (dec r0) r7 r7)
+             (f6027 (dec r0) (f6027 r0 (dec r1) r7) r7))))))
+    (let [res (f6027 4 1 r7)]
+      (println r7 res)
+      (when (not= 6 res)
+        (recur (inc r7))))))
+
+(defn main [_]
+  (run
+   {:memory (->> (io/input-stream "challenge.bin")
+                 .readAllBytes
+                 (map #(Byte/toUnsignedInt %))
+                 (partition 2)
+                 (reduce (fn [[m i] [low high]]
+                           [(assoc m i (+ (bit-shift-left high 8) low))
+                            (inc i)])
+                         [(vec (replicate 32768 0))
+                          0])
+                 first)
+    :registers [0 0 0 0 0 0 0 0]
+    :stack []
+    :head 0}))
